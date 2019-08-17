ceb05132099c0a72f8a915c1cea8ded8b6b2b589
Fernando0107 <rmfernando@outlook.com>
2019-08-16 20:03:59 -0600

    crawler.js file

---
+++ crawler.js
@@ -0,0 +1,3 @@
+var request = require('request');   // Make HTTP request 
+var cheerio = require('cheerio');   // To parse and select HTML elements on page
+var URL = require('url-parse');     // To parse Url's
